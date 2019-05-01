198e4f655fa889f00c2947a9491c4e6e07fda578
PavelMartinez <golovip2014@mail.ru>
2019-05-01 13:27:07 +0300

    Updates

---
+++ precache-manifest.b6cab1d269ebcb9622e9ca3ef4a292b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ab0338086ce48105b5e",
+    "url": "/vk-app-concurs/static/js/runtime~main.4ab03380.js"
+  },
+  {
+    "revision": "bf63a2fc59aa06764aae",
+    "url": "/vk-app-concurs/static/js/main.bf63a2fc.chunk.js"
+  },
+  {
+    "revision": "512e6190660eb7764c40",
+    "url": "/vk-app-concurs/static/js/2.512e6190.chunk.js"
+  },
+  {
+    "revision": "512e6190660eb7764c40",
+    "url": "/vk-app-concurs/static/css/2.872bf7fe.chunk.css"
+  },
+  {
+    "revision": "b30eb68da625db7ae40a359c9db3bdf5",
+    "url": "/vk-app-concurs/index.html"
+  }
+];
