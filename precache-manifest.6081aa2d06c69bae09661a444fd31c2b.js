a882f057f5dc3f1217ad6c440513887fbe10782c
PavelMartinez <golovip2014@mail.ru>
2019-05-02 23:09:35 +0300

    Updates

--- precache-manifest.6081aa2d06c69bae09661a444fd31c2b.js
+++ precache-manifest.6081aa2d06c69bae09661a444fd31c2b.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/vk-app-concurs/static/js/runtime~main.4ab03380.js"
   },
   {
-    "revision": "1b93fc39585fda183900",
-    "url": "/vk-app-concurs/static/js/main.1b93fc39.chunk.js"
+    "revision": "de9aecfe77393056c232",
+    "url": "/vk-app-concurs/static/js/main.de9aecfe.chunk.js"
   },
   {
     "revision": "c387200858e1dc4a45f9",
     "url": "/vk-app-concurs/static/js/2.c3872008.chunk.js"
   },
   {
-    "revision": "1b93fc39585fda183900",
-    "url": "/vk-app-concurs/static/css/main.10f6f155.chunk.css"
+    "revision": "de9aecfe77393056c232",
+    "url": "/vk-app-concurs/static/css/main.885620a2.chunk.css"
   },
   {
     "revision": "c387200858e1dc4a45f9",
     "url": "/vk-app-concurs/static/css/2.872bf7fe.chunk.css"
   },
   {
-    "revision": "1c76af445fa689d1c0e3d2a38686ecf2",
+    "revision": "4be6dc6c25d0d1484af83ca47e46c2b9",
     "url": "/vk-app-concurs/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/vk-app-concurs/precache-manifest.1d765478a0ff4713d60a4f807f8e4088.js"
+  "/vk-app-concurs/precache-manifest.6081aa2d06c69bae09661a444fd31c2b.js"
 );
 
 workbox.clientsClaim();
